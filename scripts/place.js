e1b4a280bd1787336600f723c3b23b8031f6bc40
aghogho <aghoghomonorien@gmail.com>
2024-05-12 04:57:03 +0100

    Place - Mobile view completed

--- scripts/place.js
+++ scripts/place.js
@@ -7,3 +7,29 @@ getYear.innerHTML = `${today.getFullYear()}`;
 lastModified.innerHTML = `Last Modification: 
 ${today.getDate()}/${today.getMonth()}/${today.getFullYear()} ${today.getHours()}:${today.getMinutes()}:${today.getSeconds()}`;
 
+
+const windChill = document.querySelector("#windChill");
+const condition = document.querySelector("#cdn");
+
+function CalWindChill(celsius, windKmh) {
+    
+    if (celsius > 10 || windKmh <= 4.8)
+    {
+        return Math.NaN;
+    }
+    
+    const windchill = (
+        13.12 + 0.6215 * celsius - 11.37 * Math.pow(windKmh, 0.16) +
+        0.3965 * celsius * Math.pow(windKmh, 0.16)
+    );
+
+    return windchill;
+}
+
+const windChillValue = Math.round(CalWindChill(10, 5));
+
+windChill.innerHTML = `${windChillValue}<sup>∘</sup>C`;
+
+function Condition(windChillValue) {
+
+}
