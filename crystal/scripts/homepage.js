f4777f57e0aca9e5f19afe0a41c5a92c6b283630
aghogho <aghoghomonorien@gmail.com>
2024-06-02 06:47:50 +0100

    First image

--- crystal/scripts/homepage.js
+++ crystal/scripts/homepage.js
@@ -4,4 +4,20 @@ const menuBtn = document.querySelector("#menu-btn");
 menuBtn.addEventListener("click", () => {
     nav.classList.toggle("open");
     menuBtn.classList.toggle("open");
-})
+});
+
+
+const imageWrapper = document.querySelector("#image-wrapper");
+const imageTop = document.querySelector("#image-top");
+const imageBottom = document.querySelector("#image-bottom");
+
+imageWrapper.addEventListener("mouseover", () => {
+    imageTop.style.opacity = "0";
+    imageBottom.style.opacity = "1";
+});
+
+imageWrapper.addEventListener('mouseout', () => {
+    imageTop.style.opacity = '1';
+    imageBottom.style.opacity = '0';
+});
+
