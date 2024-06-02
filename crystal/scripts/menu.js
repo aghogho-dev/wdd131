9d5d3a9248ba9e00683e25ae485a329f8008e476
aghogho <aghoghomonorien@gmail.com>
2024-06-02 19:35:28 +0100

    menu page

--- crystal/scripts/menu.js
+++ crystal/scripts/menu.js
@@ -16,3 +16,86 @@ menuBtn.addEventListener("click", () => {
     nav.classList.toggle("open");
     menuBtn.classList.toggle("open");
 });
+
+
+const menuItems = [
+    {
+        type: "forward",
+        name: "THE OTHER WOMAN",
+        description: "Illegal Mezcal, Laphroaig 10yr Islay Scotch, Blackened Bird’s Eye Chili Tincture, Grapefruit Bitters, Agave",    
+    },
+    {
+        type: "forward",
+        name: "TAJ EMINENCE",
+        description: "Tanduay Rum, Chinola, Hibiscus, Rose Combier, Lime",
+    },
+    {
+        type: "forward",
+        name: "TAPACHE",
+        description: "Dulce Vida, Jalapeno-Pineapple, Watermelon, Lime, Agave, Blue Curacao",
+    },
+    {
+        type: "forward",
+        name: "GOLD NEGRONI",
+        description: "Jaisalmer Gin, Spiced Saffron Liqueur, Dry Vermouth",  
+    },
+    {
+        type: "forward",
+        name: "SUITORS",
+        price: 20,
+        description: "Hendricks Flora, Aperol, Strawberry, Rosemary, Lemon, Eau de Rose, Simple, Prosecco",
+    },
+
+    {
+        type: "shy",
+        name: "THE LYCHEE LIBRA",
+        description: "Rosemary Infused Butterfly Pea Tea, Lychee, Lemonade",
+    },
+
+    {
+        type: "shy",
+        name: "MANGO MALAI",
+        description: "Mango Puree, Yogurt",
+    },
+
+    {
+        type: "shy",
+        name: "KOKUM MOJITO",
+        description: "Kokum, Mint, Lime, Simple, Soda",
+    },
+
+    {
+        type: "shy",
+        name: "COCO-LICIOUS",
+        description: "Coconut Puree, Lime, Simple, Cardamom",
+    },
+];
+
+const getForward = document.querySelector(".forward");
+const getShy = document.querySelector(".shy");
+
+
+menuItems.forEach(item => {
+    const createDiv = document.createElement("div");
+    const createNameP = document.createElement("p");
+    const createDescP = document.createElement("p");
+
+    createNameP.textContent = item.name;
+    createNameP.classList.add("name-items");
+
+    createDescP.textContent = item.description;
+
+
+    if (item.type === "forward")
+    {
+        createDiv.appendChild(createNameP);
+        createDiv.appendChild(createDescP);
+        getForward.appendChild(createDiv);
+    }
+    else if (item.type === "shy")
+    {
+        createDiv.appendChild(createNameP);
+        createDiv.appendChild(createDescP);
+        getShy.appendChild(createDiv);
+    }
+})
