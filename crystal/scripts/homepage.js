670946ac1d34431f8a470e3ed8f2373d2b581e62
aghogho <aghoghomonorien@gmail.com>
2024-06-02 15:57:26 +0100

    footer

--- crystal/scripts/homepage.js
+++ crystal/scripts/homepage.js
@@ -21,3 +21,12 @@ imageWrapper.addEventListener('mouseout', () => {
     imageBottom.style.opacity = '0';
 });
 
+const getYear = document.querySelector("#currentyear");
+const lastModified = document.querySelector("#lastModified")
+const today = new Date();
+
+getYear.innerHTML = `${today.getFullYear()}`;
+
+lastModified.innerHTML = `Last Modification: 
+${today.getDate()}/${today.getMonth()}/${today.getFullYear()} ${today.getHours()}:${today.getMinutes()}:${today.getSeconds()}`;
+
