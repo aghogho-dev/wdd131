82bf3e0290b94684060d4db3f2480760274d9647
aghogho <aghoghomonorien@gmail.com>
2024-06-05 11:39:17 +0100

    exam

---
+++ exams/scripts/exam.js
@@ -0,0 +1,34 @@
+// msg = document.querySelector("p");
+// msg1 = document.querySelector("span");
+
+// msg.textContent = "You Win!";
+// msg1.innerHTML = "you win!";
+
+
+// const msg = document.querySelector(".message");
+
+// const msg = document.querySelector("#message");
+// msg.textContent = "You";
+
+// const msg1 = document.getElementById("message");
+// msg1.textContent = "Yes";
+
+// let scores = [56, 78, 95, 77, 65, 86];
+// let target = scores.indexOf(78);
+
+// let wards = 20;
+// const ward = `<strong>Unit Name</strong>: ${wards}`;
+// msg.innerHTML = ward;
+
+// const scores = [99, 88, 93, 70, 84, 69];
+
+// const out = scores.filter(total => total < 70);
+
+// const section = document.createElement('section');
+// const h3 = document.createElement('h3');
+// h3.textContent = 'Relief Society';
+
+// section.appendChild(h3);
+
+// const countries = ["Canada", "Chili", "Colombia", "Croatia", "Costa Rica"];
+// const countriesLong = countries.filter((country) => country.length > 7);
